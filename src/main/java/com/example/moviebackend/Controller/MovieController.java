5cb34bceddb6a3a74f5098373e830b6fdf938a6c
Amalpm7 <amal.p@nestgroup.net>
2022-10-19 14:36:42 +0530

    Dao

--- src/main/java/com/example/moviebackend/Controller/MovieController.java
+++ src/main/java/com/example/moviebackend/Controller/MovieController.java
@@ -1,13 +1,24 @@
 package com.example.moviebackend.Controller;
 
 
+import com.example.moviebackend.Model.MovieModel;
+import com.example.moviebackend.dao.MovieDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MovieController {
-    @PostMapping("/movieAdd")
-    public String movie(){
-        return "Test";
+
+    @Autowired
+    private MovieDao dao;
+
+    @CrossOrigin (origins = "*")
+    @PostMapping(path =  "/movieAdd", consumes = "application/json", produces = "application/json")
+    public String movie(@RequestBody MovieModel movie){
+        dao.save(movie);
+        return "{status:'success'}";
     }
 }
